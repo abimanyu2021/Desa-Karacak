f3d39eb5b40e559e7f8e5cfb0c21657d89145a85
Desa Karacak <148981299+abimanyu2021@users.noreply.github.com>
2023-10-29 12:40:31 +0700

    Update HomeView.vue
    home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,44 +12,65 @@ import Sidebar from "../components/Sidebar.vue";
     <Sidebar />
 
     <div class="flex-1 p-10">
-      <p class="text-3xl font-semibold">Visi & Misi Desa Karacak</p>
-      <p class="text-lg font-semibold">Visi Desa Karacak</p>
-
-      
+      <p class="text-3xl font-semibold">Visi & Misi Desa Contoh TUgas Akhir</p>
+      <p class="text-lg font-semibold">Visi</p>
       <p>
-        “Menciptakan Tata Kelola Pemerintahan Yang Amanah, Jujur, Transparan dan Bertanggungjawab dalam Rangka Mewujudkan Masyarakat Desa Karacak Yang Maju, Adil, Aman, dan Mandiri”
+        “Terwujudnya Desa Setu Yang Sejahtera dan Mandiri, ditopang Kinerja
+        Aparatur yang Profesional dalam Kerangka Budaya Bangsa didasari Keimanan
+        dan Ketaqwaan”
       </p>
-      
+      <p class="text-lg font-semibold">Misi</p>
       <ul>
         <li>
-          1. Melakukan perbaikan sistem kinerja aparatur pemerintahan desa guna meningkatkan kualitas pelayanan kepada masyarakat;
+          1. Mengembangkan system Pemerintahan Desa yang bersih dan profesional
+          serta berorientasi pada pelayanan publik dengan prinsif kerja ikhlas,
+          lakukan sekarang dan siap turun ke lapangan;
         </li>
         <li>
-          2. Menyelenggarakan pemerintahan yang baik, bebas dari bentuk-bentuk penyelewengan;
+          2. Meningkatkan dan memantapkan system perencanaan pembangunan desa
+          melalui pengoptimalan mekanisme musyawarah pembangunan desa dengan
+          melibatkan seluruh steackholder yang ada;
         </li>
         <li>
-          3. Menyelenggarakan urusan pemerintahan desa secara terbuka dan bertanggungjawab sesuai dengan peraturan perundang-undangan;
+          3. Membuat skala prioritas untuk kegiatan-kegiatan pembangunan yang
+          akan dilaksanakan baik pembangunan fisik atau non fisik, seperti
+          infrastruktur, perekonomian, keagamaan, Pendidikan dan kesehatan;
         </li>
         <li>
-          4. Meningkatkan perekonomian masyarakat berbasis pertanian, peternakan, perikanan melalui kelompok dan UKM yang produktif  sesuai dengan potensi Desa dan peluang pasar;
+          4. Meningkatkan kemampuan dan peran serta aktif masyarakat dalam
+          mengelola pembangunan berkelanjutan secara efisisen dan efektif dengan
+          memanfaatkan potensi sumber daya yang ada, baik sumber daya alam
+          maupun sumberdaya manusia;
         </li>
         <li>
-          5. Menyelenggarakan pelatihan dan penyuluhan bagi kelompok pertanian, peternakan, perikanan, UKM dan bagi masyarakat pada umumnya sesuai dengan kebutuhan;
+          5. Meningkatkan kemampuan dan pemberdayaan Lembaga-lembaga
+          kemasyarakatan yang ada didesa atau lebih mendinamisasi dan mendukung
+          seluruh proses pembangunan;
         </li>
         <li>
-          6. Membentuk Badan Usaha Milik Desa (BUMDes) dan unit-unitnya sebagai lembaga yang mengelola produktivitas usaha masyarakat;
+          6. Mendorong majunya Pendidikan formal maupun nonformal seperti
+          pesantren, LPK, diklat serta penyuluhan pertanian yang mudah diakses
+          dan dinikmati seluruh warga masyarakat, guna menghasilkan insan
+          intelektual, berakhlak mulia, inovatif dan entrepreneur;
         </li>
         <li>
-          7. Menjalin kerja sama dengan semua pihak dalam rangka peningkatan mutu kesejahteraan masyarakat;
+          7. Menyalurkan segala bentuk bantuan dari Pemerintah Kabupaten,
+          Provinsi dan Pusat sesuai dengan peruntukannya dan mencegah terjadinya
+          penyimpangan dan kebocoran;
         </li>
         <li>
-          8. Membangun pola kehidupan masyarakat yang sehat melalui peningkatan kualitas kegiatan posyandu balita, posyandu lansia, kebersihan lingkungan secara umum dan pembangunan rumah tidak layak huni berdasarkan skala prioritas;
+          8. Menjalin sinergitas dengan instansi atau Lembaga terkait dan pihak
+          swasta guna optimalisasi suatu tujuan lebih baik dan lebih maksimal;
         </li>
         <li>
-          9. Mengembalikan fungsi organisasi-organisasi masyarakat Desa Karacak untuk membantu pemerintah Desa dalam penyelenggaraan Pemerintahan atas dasar kepentingan masyarakat pada umumnya;
+          9. Responsive dan aspiratif atas segala bentuk kebutuhan atau
+          keinginan dan permasalahan yang terjadi ditengah masyarakat yang
+          majemuk dan dinamis;
         </li>
         <li>
-          10. Mengorganisir kaum muda Desa Karacak  dan meningkatkan perannya sebagai kader pembangunan dan kader pemimpin masa depan melalui Karang Taruna.
+          10. Membina dan memantapkankehidupan gotong royongsebagai basis dan
+          modal dasar pembangunan desa, yang pada hakikatnyajuga merupakan
+          karakter dasar dari masyarakat desa dan juga bangsa Indonesia.
         </li>
       </ul>
     </div>
