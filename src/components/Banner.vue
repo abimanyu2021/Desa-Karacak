cdfaef9c613f6ff385a680ec1cbdf67fbc27dd60
Desa Karacak <148981299+abimanyu2021@users.noreply.github.com>
2023-10-29 14:27:54 +0700

    Update link you tube

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -15,7 +15,7 @@
       <iframe
         width="480"
         height="260"
-        src="https://www.youtube.com/embed/yzfvnYg2ELw?si=jaKHeIL2nVjhdeqa"
+        src="https://www.youtube.com/embed/wIaVK5QnTrM?si=JtKWmXWfdMQgHM2D"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
